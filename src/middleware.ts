8ce319bbd014df0e4533a8b9d68f3ff78728df74
bangbang93 <bangbang93@163.com>
2020-12-22 10:32:34 +0800

    fix type

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,16 +15,17 @@ export type ActionType = DocumentMiddlewareType | QueryMiddlewareType | Aggregat
 
 type R<T> = T | Promise<T>
 type DocumentHookFunction<T> = (this: DocumentType<T>, error?: Error, doc?: DocumentType<T>, next?: Fn) => R<unknown>
-type QueryHookFunction<T> = (this: Query<T>, error?: Error, doc?: Query<T>, next?: Fn) => R<unknown>
+type QueryHookFunction<T extends Document> =
+  (this: Query<T, T>, error?: Error, doc?: Query<T, T>, next?: Fn) => R<unknown>
 type AggregateHookFunction<T> = (this: Aggregate<T>, error?: Error, doc?: Aggregate<T>, next?: Fn) => R<unknown>
 type ModelHookFunction<T extends Document> = (this: Model<T>, error?: Error, doc?: Model<T>, next?: Fn) => R<unknown>
-type HookFunction<T> = T extends Document ? ModelHookFunction<T>
-  : DocumentHookFunction<T> | QueryHookFunction<T> | AggregateHookFunction<T>
+type HookFunction<T> = T extends Document ? ModelHookFunction<T> | QueryHookFunction<T>
+  : DocumentHookFunction<T> | AggregateHookFunction<T>
 
 
 export function middleware<T>(actionType: DocumentMiddlewareType, hookType: HookType,
   hookFunction: DocumentHookFunction<T>)
-export function middleware<T>(actionType: QueryMiddlewareType, hookType: HookType,
+export function middleware<T extends Document>(actionType: QueryMiddlewareType, hookType: HookType,
   hookFunction: QueryHookFunction<T>)
 export function middleware<T>(actionType: AggregateMiddlewareType, hookType: HookType,
   hookFunction: AggregateHookFunction<T>)
